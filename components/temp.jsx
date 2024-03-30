7db45c6fb58b7c2ff863ffa3be28482c0110e8ac
Pratykash <113509128+Pratykash-ops@users.noreply.github.com>
2024-03-30 17:51:51 +0000

    ver. 0

---
+++ components/temp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function temp() {
+  return (
+    <div className='flex items-center justify-center h-screen w-screen bg-gradient-to-r from-purple-500 to-violet-700 text-white flex-col'>
+        <h2 className="text-6xl animate-pulse font-semibold">
+          Project 0
+        </h2>
+        <span className='mt-5'>comming soon</span>
+    </div>
+  )
+}
